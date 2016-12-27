6d24a1db23c81966d4b2fd422e56ddf7b6a65359
audiofreak9 <audiofreak9@gmail.com>
2016-12-26 23:15:08 -0500

    Update devices.py

--- devices.py
+++ devices.py
@@ -4,15 +4,18 @@ for device in requests.get('http://localhost/api/devices').json():
     if device['offUrl'].find('heyu') != -1:
         url = "http://localhost/api/c/lights/%s/bridgeupdatestate" % device['id']
         heyu = device['offUrl'].replace("foff", "dimlevel").replace("off","dimlevel")
-        proc = subprocess.Popen([heyu], stdout=subprocess.PIPE, shell=True)
-        (dimlevel, err) = proc.communicate()
-        dimlevel = int(dimlevel)
-        if dimlevel > 0:
+        if heyu.find('dimlevel') != -1:
+            proc = subprocess.Popen([heyu], stdout=subprocess.PIPE, shell=True)
+            (dimlevel, err) = proc.communicate()
+            dimlevel = int(dimlevel)
+            if dimlevel > 0:
                 dimlevel = ((dimlevel*255)/100)
                 state = "true"
-        else:
+            else:
                 state = "false"
-        print "Device ID %s set to %s at dim level %s (%s)" % (device['id'], state, dimlevel, device['name'])
-        r = requests.put(url, json={"on":state, "bri":dimlevel})
+            print "Device ID %s set to %s at dim level %s (%s)" % (device['id'], state, dimlevel, device['name'])
+            r = requests.put(url, json={"on":state, "bri":dimlevel})
+        else:
+            print "Device ID %s is an X10 command (%s)" % (device['id'], device['name'])
     else:
         print "Device ID %s not an X10 device (%s)" % (device['id'], device['name'])
