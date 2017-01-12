cf44b80779270420ea5a2ddde8f8957f7b1d4151
audiofreak9 <audiofreak9@gmail.com>
2017-01-11 19:32:41 -0500

    Update devices.py

--- devices.py
+++ devices.py
@@ -3,9 +3,9 @@ import urllib2, subprocess, requests, time
 timestamp = time.strftime("Time: %H:%M, Date: %Y%m%d, ")
 print timestamp
 for device in requests.get('http://localhost/api/devices').json():
-    if device['offUrl'].find('heyu') != -1:
+    if device['onUrl'].find('heyu') != -1:
         url = "http://localhost/api/c/lights/%s/bridgeupdatestate" % device['id']
-        chtxt = device['offUrl'].replace("foff", "dimlevel").replace("off","dimlevel")
+        chtxt = device['onUrl'].replace("python", "").replace(",", "").replace(":", "").replace("\"", "").replace("item", "").replace("typenull", "").replace("typecmdDevice", "").replace("[{", "").replace("}]", "").replace("fon", "dimlevel").replace("on","dimlevel")
         if chtxt.find('dimlevel') != -1:
             proc = subprocess.Popen([chtxt], stdout=subprocess.PIPE, shell=True)
             (dimlevel, err) = proc.communicate()
@@ -15,9 +15,13 @@ for device in requests.get('http://localhost/api/devices').json():
                 state = "true"
             else:
                 state = "false"
-            print "Device ID %s set to %s at dim level %s (%s)" % (device['id'], state, dimlevel, device['name'])
-            r = requests.put(url, json={"on":state, "bri":dimlevel})
+            payload = "{\"on\":%s" % state
+            if state == "true":
+                payload = "%s, \"bri\":%s" % (payload, dimlevel)
+            payload = "%s}" % payload
+            print "Device ID %s set to %s (%s)" % (device['id'], payload, device['name'])
+            r = requests.put(url, data=payload)
         else:
             print "Device ID %s is an X10 command (%s)" % (device['id'], device['name'])
     else:
-        print "Device ID %s not an X10 device (%s)" % (device['id'], device['name'])
+        print "Device ID %s not an X10 device (%s) - [%s]" % (device['id'], device['name'], device['mapType'])
